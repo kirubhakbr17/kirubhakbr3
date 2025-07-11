b726bf244c10d90f8941d4dd78cb0daad9afbc49
kirubhakbr17 <kirubhakbr17@gmail.com>
2025-07-10 23:37:18 -0930

    new react file createda

--- src/assets/projects/bootstrap.jsx
+++
@@ -1,8 +0,0 @@
-import './cdnlink'
-export default function Bootstrap(){
-    return <div className style={{backgroundColor:"red"}}>
-<p>this is bootstrap using in react</p>
-
-
-    </div>
-}
--- src/assets/projects/src/components/Picture.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Picture() {
-  return <div id="picture"></div>;
-}
-
-export default Picture;
